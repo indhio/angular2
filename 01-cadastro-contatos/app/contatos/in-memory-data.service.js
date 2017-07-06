fe0c057510220ff275a73164cd6e0ca666daf908
indhio <indhio@gmail.com>
2017-07-06 12:53:20 -0300

    01-contatos
    01-contatos

--- 01-cadastro-contatos/app/contatos/in-memory-data.service.js
+++ 01-cadastro-contatos/app/contatos/in-memory-data.service.js
@@ -1,13 +1,14 @@
 "use strict";
 class InMemoryDataService {
     createDb() {
-        let contatos = Contato[] = [
+        let contatos = [
             { id: 1, nome: 'Fulano', email: '@@@', telefone: '1111' },
             { id: 2, nome: 'Beltrano', email: '@@@', telefone: '222' },
             { id: 3, nome: 'Ciclano', email: 'c@com', telefone: '3333' },
             { id: 4, nome: 'Haitinano', email: 'h@h.com', telefone: '333334444' },
             { id: 5, nome: 'Xixpetano', email: 'x@x.com', telefone: '55555' }
         ];
+        return { contatos };
     }
 }
 exports.InMemoryDataService = InMemoryDataService;
